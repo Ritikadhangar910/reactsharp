e84683ad8ec34bce91dcd1d25789f3f4e908cb41
dhangarritika <dhangarritika90@gmail.com>
2023-08-20 12:49:39 +0530

    pass obj of array as an prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
-  const expenselocation = "Delhi";
+  const expenses = [
+    { title: "Car insurense", amount: 234.56, date: new Date(2022, 10, 4) },
+    { title: "Bike insurense", amount: 987.56, date: new Date(2000, 3, 6) },
+    { title: "Food", amount: 36, date: new Date(2022, 6, 4) },
+    { title: "movie", amount: 335.0, date: new Date(2021, 23, 5) },
+  ];
   return (
     <div className="App">
-      <ExpenseItem city={expenselocation} />
+      {expenses.map((expense, i) => (
+        <div key={i}>
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            d={expense.date}
+          />
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,10 +2,10 @@ function ExpenseItem(props) {
   return (
     <>
       <h2>Expense Items</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
-      <p>expend money : {props.city}</p>
+
+      <p>Title: {props.title}</p>
+      <p>Amount: {props.amount}</p>
+      <p>Date: {props.d.toISOString()}</p>
     </>
   );
 }
