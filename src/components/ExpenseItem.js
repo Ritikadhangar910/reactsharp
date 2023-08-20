21d82f18b4b6c94048fcbbbc1dedbb43a5ba35c6
dhangarritika <dhangarritika90@gmail.com>
2023-08-20 13:01:52 +0530

    edit props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
 function ExpenseItem(props) {
+  const month = props.d.toLocaleString("en-US", { month: "long" });
+  const day = props.d.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.d.getFullYear();
   return (
     <>
       <h2>Expense Items</h2>
 
       <p>Title: {props.title}</p>
       <p>Amount: {props.amount}</p>
-      <p>Date: {props.d.toISOString()}</p>
+      <p>
+        month: {month}, day: {day}, year: {year}
+      </p>
     </>
   );
 }
