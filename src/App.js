2b8ab4a43fcec86f83268c59cf0c738deb460d6a
dhangarritika <dhangarritika90@gmail.com>
2023-08-20 12:29:12 +0530

    pass expense as an prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenselocation = "Delhi";
   return (
     <div className="App">
-      <ExpenseItem />
+      <ExpenseItem city={expenselocation} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <>
       <h2>Expense Items</h2>
       <p>Food Rs 10</p>
       <p>Petrol Rs 100</p>
       <p>Movies Rs 200</p>
+      <p>expend money : {props.city}</p>
     </>
   );
 }
