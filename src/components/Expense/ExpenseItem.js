51d5e1e0876e7ce2d4ce1622b95901b206726e14
dhangarritika <dhangarritika90@gmail.com>
2023-08-21 11:18:57 +0530

    delete card

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,6 +3,11 @@ import ExpenseDetails from "./ExpenseDetails";
 import "../../App.css";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
+  function deleteExpense(e) {
+    const grabelem = e.target.parentNode;
+    const getdiv = e.target.parentNode.parentNode;
+    getdiv.removeChild(grabelem);
+  }
   return (
     <>
       <Card className="cover">
@@ -15,6 +20,7 @@ const ExpenseItem = (props) => {
           location={props.location}
           title={props.title}
         />
+        <button onClick={deleteExpense}>delete</button>
       </Card>
     </>
   );
